5eea54a8b5f02822a08c4418053ba2257e26ef4d
MateoHoyos <mateo.hoyos@udea.edu.co>
2020-10-17 16:49:15 -0500

    codificacion3

--- Parcial1/main.cpp
+++ Parcial1/main.cpp
@@ -17,34 +17,49 @@ sivo.
 
 4. Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los
 cañones defensivo y ofensivo puedan ser destruidos.
+
+
+************************************
+************************************
+Al estar en la ofensiva, se exactamente la posicion en x y h a la cual esta el cañon ofensivo
+
+
 */
 void Generar_disparos_ofensivos();
 void Generar_disparos_defensivos();
 
 
 //variables globales
+int disparos_ofensivos[3]={0};
+int disparos_defensivos[3]={0};
+
 float g=9.81;
+float pi=3.141592;
 
 float x_0=0;
 float y_0=0;
 float vx0=0;
 float vy0=0;
+float t0=2.5;
 
+float x0_0=0;
 float v0_0=0;
 float H0=1;
 float grados0=0;
+float rad0=0;
 //***************************
 
-float x_H=0;
-float y_H=0;
-float vxH=0;
-float vyH=0;
-
-float HD=2;
-float gradosH=0;
+float x_D=0;
+float y_D=0;
+float vxD=0;
+float vyD=0;
 float tD=2.5;
-float v0_D=0;
 
+float x0_D=0;
+float v0_D=0;
+float HD=1;
+float gradosD=0;
+float radD=0;
 int n=0;
 //***************************
 
@@ -74,6 +89,7 @@ int main()
             break;
 
             case 2:
+            Generar_disparos_defensivos();
             break;
 
             case 3:
@@ -96,9 +112,29 @@ int main()
     return 0;
 }
 
+//infiltrado
 void Generar_disparos_ofensivos(){
-    cout<<"Ingrese velocidad inicial: ";cin>>v0_0;
-    cout<<"Ingrese grados_0: ";cin>>grados0;
+
+    for(int i=0;i<3;i++){
+        cout<<"Ingrese velocidad inicial: ";cin>>v0_0;
+        cout<<"Ingrese grados_0: ";cin>>grados0;
+        rad0=grados0*(pi/180);
+        cout<<"Ingrese ditancia en y: ";cin>>H0;
+
+        vx0=v0_0+cos(rad0);
+        vy0=v0_0*sin(rad0)-g*t0;
+
+        x_0=0+vx0*t0;
+        y_0=H0+vy0*t0-(g*t0*t0)/2;
+    }
+
+
+}
+
+
+
+
+void Generar_disparos_defensivos(){
 
 
 }
